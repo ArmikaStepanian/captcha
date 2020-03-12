9d42262bc94e892cdf074826d420c6f4aa240a19
Stepanian <armikastepanian@gmail.com>
2020-03-12 10:28:30 -0400

    add extra check

--- src/main/java/com/stepanian/captcha/web/Controller.java
+++ src/main/java/com/stepanian/captcha/web/Controller.java
@@ -1,6 +1,7 @@
 package com.stepanian.captcha.web;
 
 import com.stepanian.captcha.exceptions.IncorrectLengthException;
+import com.stepanian.captcha.exceptions.IncorrectSizeException;
 import com.stepanian.captcha.services.CreateCaptchaImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,9 +36,12 @@ public class Controller {
     public @ResponseBody
     byte[] getCaptcha(@RequestParam String value,
                       @RequestParam(required = false) Integer width,
-                      @RequestParam(required = false) Integer height) throws IOException, IncorrectLengthException {
+                      @RequestParam(required = false) Integer height) throws Exception {
         if (value.length() < 4 || value.length() > 8) {
             throw new IncorrectLengthException("Value: size must be between 4 and 8");
+        }
+        if (width == 0 || height == 0 || width / height < 1) {
+            throw new IncorrectSizeException("Width: must be > 0, Height: must be > 0, width/height must be >= 1");
         }
         return createCaptchaImageService.getCaptcha(value, width, height);
     }
